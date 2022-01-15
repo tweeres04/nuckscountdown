81c8c38ea0af6ad0f61c9652291603688daf7e2b
Tyler Weeres <tweeres04@gmail.com>
2022-01-15 14:27:01 -0800

    Stale while revalidate cache strategy

--- src/NucksCountdown.js
+++ src/NucksCountdown.js
@@ -1,81 +1,85 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import dateFormat from 'date-fns/format';
-import addMonths from 'date-fns/add_months';
-import isPast from 'date-fns/is_past';
-import countdown from 'countdown';
-import idbKeyval from 'idb-keyval';
+import React, { Component } from 'react'
+import axios from 'axios'
+import dateFormat from 'date-fns/format'
+import addMonths from 'date-fns/add_months'
+import isPast from 'date-fns/is_past'
+import countdown from 'countdown'
+import idbKeyval from 'idb-keyval'
 
-import Logo from './NucksLogo';
-import getOpposingTeamName from './getOpposingTeamName';
+import Logo from './NucksLogo'
+import getOpposingTeamName from './getOpposingTeamName'
 
 const strings = {
 	noGame: 'No game scheduled',
 	live: 'Nucks are live!',
 	puckDrop: 'Puck is about to drop!',
-	countdown: gameDate =>
+	countdown: (gameDate) =>
 		`${countdown(gameDate).toString()} till the Nucks play next`,
-	dateFormat: 'YYYY-MM-DD'
-};
+	dateFormat: 'YYYY-MM-DD',
+}
 
 function getNextGame(dates) {
 	let [
 		{
-			games: [game]
-		}
-	] = dates;
+			games: [game],
+		},
+	] = dates
 	const {
-		status: { abstractGameState }
-	} = game;
-	return abstractGameState === 'Final' ? dates[1] && dates[1].games[0] : game;
+		status: { abstractGameState },
+	} = game
+	return abstractGameState === 'Final' ? dates[1] && dates[1].games[0] : game
 }
 
 async function getGameFromNhlApi() {
-	const today = dateFormat(new Date(), strings.dateFormat);
+	const today = dateFormat(new Date(), strings.dateFormat)
 	const sixMonthsFromNow = dateFormat(
 		addMonths(new Date(), 6),
 		strings.dateFormat
-	);
+	)
 	const {
-		data: { dates }
+		data: { dates },
 	} = await axios(
 		`https://statsapi.web.nhl.com/api/v1/schedule?startDate=${today}&endDate=${sixMonthsFromNow}&teamId=23`
-	);
+	)
 
-	const game = dates.length === 0 ? null : getNextGame(dates);
-	idbKeyval.set('game', game);
-	return game;
+	const game = dates.length === 0 ? null : getNextGame(dates)
+	idbKeyval.set('game', game)
+	return game
 }
 
 class NucksCountdownContainer extends Component {
-	state = { loading: true, game: null, now: new Date() };
+	state = { loading: true, game: null, now: new Date() }
 	async componentDidMount() {
-		let game = await idbKeyval.get('game');
-		let { gameDate } = game || {};
+		let game = await idbKeyval.get('game')
 
-		gameDate = new Date(gameDate);
-		game = !game || isPast(gameDate) ? await getGameFromNhlApi() : game;
+		if (game) {
+			this.setState({ loading: false, game })
+		}
 
-		this.setState({ loading: false, game });
+		getGameFromNhlApi().then((game) => {
+			setTimeout(() => {
+				this.setState({ loading: false, game })
+			}, 1000)
+		})
 
 		this.intervalHandle = setInterval(() => {
-			this.setState({ now: new Date() });
-		}, 1000);
+			this.setState({ now: new Date() })
+		}, 1000)
 	}
 	render() {
-		const { loading, game } = this.state;
-		return loading || <NucksCountdown game={game} />;
+		const { loading, game } = this.state
+		return loading || <NucksCountdown game={game} />
 	}
 	componentWillUnmount() {
-		clearInterval(this.intervalHandle);
+		clearInterval(this.intervalHandle)
 	}
 }
 
 function NucksCountdown({ game }) {
-	const { status: { abstractGameState } = {} } = game || {};
-	let { gameDate, teams } = game || {};
+	const { status: { abstractGameState } = {} } = game || {}
+	let { gameDate, teams } = game || {}
 
-	gameDate = gameDate && new Date(gameDate);
+	gameDate = gameDate && new Date(gameDate)
 
 	const countdownString = !game
 		? strings.noGame
@@ -83,9 +87,9 @@ function NucksCountdown({ game }) {
 		? strings.live
 		: abstractGameState === 'Preview' && isPast(gameDate)
 		? strings.puckDrop
-		: strings.countdown(gameDate);
+		: strings.countdown(gameDate)
 
-	const opposingTeamName = getOpposingTeamName(teams);
+	const opposingTeamName = getOpposingTeamName(teams)
 
 	return (
 		<div className="App">
@@ -103,7 +107,7 @@ function NucksCountdown({ game }) {
 				<div className="opponent">vs {opposingTeamName}</div>
 			)}
 		</div>
-	);
+	)
 }
 
-export default NucksCountdownContainer;
+export default NucksCountdownContainer
