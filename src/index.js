e45cd546a1182eb79a450c055afb208e6950264a
Tyler Weeres <tweeres04@gmail.com>
2022-03-05 15:19:15 -0800

    Add landing page; Move to next.js

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import NucksCountdown from './NucksCountdown';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<NucksCountdown />, document.getElementById('root'));
-registerServiceWorker();
