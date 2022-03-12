9bd8b6843597930826955e429d68737fbd3b45c4
Tyler Weeres <tweeres04@gmail.com>
2022-03-12 08:14:39 -0800

    PWA

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
+const withPwa = require('next-pwa')
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withPwa({
 	reactStrictMode: true,
 	typescript: {
 		ignoreBuildErrors: true,
 	},
-}
+	pwa: {
+		dest: 'public',
+		disable: process.env.NODE_ENV != 'production',
+	},
+})
 
 module.exports = nextConfig
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,12 +6,28 @@ function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<>
 			<Head>
-				<title>Nucks Countdown</title>
+				<meta charSet="utf-8" />
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1, shrink-to-fit=no"
+				/>
+				<meta name="theme-color" content="rgb(4, 30, 66)" />
 				<meta
 					name="description"
-					content="A quick and beautiful way to see when the Vancouver Canucks play their next game"
+					content="A slick looking countdown you can save to your home screen so you can always be amped up for the next game. Loads instantly."
 				/>
-				<link rel="icon" href="/nucks.png" />
+				<link rel="manifest" href="/manifest.json" />
+				<link rel="shortcut icon" href="/nucks.png" />
+				// All the apple bullshit
+				<meta name="apple-mobile-web-app-title" content="Nucks Countdown" />
+				<link rel="apple-touch-icon" href="%PUBLIC_URL%/nucks.png" />
+				<link rel="apple-touch-startup-image" href="%PUBLIC_URL%/nucks.png" />
+				<meta name="apple-mobile-web-app-capable" content="yes" />
+				<meta name="apple-mobile-web-app-status-bar-style" content="black" />
+				<title>
+					Quickly find out when the Vancouver Canucks play next - Nucks
+					Countdown
+				</title>
 			</Head>
 			<Component {...pageProps} />
 		</>
