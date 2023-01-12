bde9c28cbd1d7374d5de11152e97c6d700bdbc88
Tyler Weeres <tweeres04@gmail.com>
2023-01-11 16:25:41 -0800

    Try to get rid of the en path in the url

--- next.config.js
+++ next.config.js
@@ -2,10 +2,6 @@ const withPwa = require('next-pwa')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = withPwa({
-	i18n: {
-		locales: ['en'],
-		defaultLocale: 'en',
-	},
 	reactStrictMode: true,
 	typescript: {
 		ignoreBuildErrors: true,
