2b0d19db74a408ad9c1c5cc8c8374e908e08a43b
Tyler Weeres <tweeres04@gmail.com>
2023-03-15 12:14:35 -0700

    Upgrade packages' minor versions

--- next.config.js
+++ next.config.js
@@ -1,14 +1,13 @@
-const withPwa = require('next-pwa')
+const withPwa = require('next-pwa')({
+	dest: 'public',
+	disable: process.env.NODE_ENV != 'production',
+})
 
 /** @type {import('next').NextConfig} */
 const nextConfig = withPwa({
 	reactStrictMode: true,
 	typescript: {
 		ignoreBuildErrors: true,
-	},
-	pwa: {
-		dest: 'public',
-		disable: process.env.NODE_ENV != 'production',
 	},
 	redirects: async () => [
 		{
