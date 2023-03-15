393138e8bc86b8d086368989eca2adcaad672529
Tyler Weeres <tweeres04@gmail.com>
2023-03-15 07:13:50 -0700

    Cite source for UA detection

--- pages/[team]/countdown.tsx
+++ pages/[team]/countdown.tsx
@@ -150,6 +150,7 @@ function InstallNotification({
 	}, [])
 
 	useEffect(() => {
+		// From https://developer.mozilla.org/en-US/docs/Web/HTTP/Browser_detection_using_the_user_agent
 		setIsIos(
 			/Mobi/.test(window.navigator.userAgent) &&
 				/AppleWebKit/.test(window.navigator.userAgent) &&
